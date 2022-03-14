b9b4d51308480e0dc14b8de40ba4f097b347d57c
Nina Bazete <96876762+ninabazete@users.noreply.github.com>
2022-03-14 19:54:08 +0100

    Add files via upload

--- main.js
+++ main.js
@@ -1,8 +1,23 @@
 function playSound (idElementAudio) {
-    document.querySelector(idElementAudio).play();
+    const element = document.querySelector(idElementAudio);
+
+    if (element != null && element.localName === 'audio') {
+            element.play();
+        } else {
+            console.log("Sorry, element not founded!");
+        }
 };
 
 const keyboardList = document.querySelectorAll('.keyboard');
 
 
 keyboardList.forEach(it => it.onclick = () => playSound(`#sound_${it.classList[1]}`));
+
+/*keyboardList.onkeydown = (e) => {
+    if (e.code === 'Space' || e.code === 'Enter') {
+        keyboardList.classList.add('ativa');
+    }
+    keyboardList.onkeyup = () => {
+        keyboardList.classList.remove('ativa';)
+    };
+}  */ 
