2f02ed27b7e6a0a4eb73d75ab3074162c1b98446
Nina Bazete <96876762+ninabazete@users.noreply.github.com>
2022-03-14 19:54:54 +0100

    Add files via upload

--- main.js
+++ main.js
@@ -13,11 +13,3 @@ const keyboardList = document.querySelectorAll('.keyboard');
 
 keyboardList.forEach(it => it.onclick = () => playSound(`#sound_${it.classList[1]}`));
 
-/*keyboardList.onkeydown = (e) => {
-    if (e.code === 'Space' || e.code === 'Enter') {
-        keyboardList.classList.add('ativa');
-    }
-    keyboardList.onkeyup = () => {
-        keyboardList.classList.remove('ativa';)
-    };
-}  */ 
